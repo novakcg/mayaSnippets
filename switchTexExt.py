cce51b89ef15da4b8fe622968c4dd81c70bd28bd
novakcg <mnovakcg@gmail.com>
2021-05-23 14:00:23 -0700

    Update
    Loop through all selected objects.

--- switchTexExt.py
+++ switchTexExt.py
@@ -7,14 +7,16 @@ gExt = raw_input()
 #Get Selection
 getSel = cmds.ls(sl = True, dag = True)
 
-#Get Shading Group & Connected Materials
-gSGrp = cmds.listConnections(getSel[1], type='shadingEngine') 
-gMtl  = cmds.ls(cmds.listConnections(gSGrp), mat = True)
+#Loop through selected objects
+for obj in getSel:
+    #Get Shading Group & Connected Materials
+    gSGrp = cmds.listConnections(getSel[obj], type='shadingEngine') 
+    gMtl  = cmds.ls(cmds.listConnections(gSGrp), mat = True)
 
-#Loop Material Textures & Switch File Extensions
-for i in gMtl:
-    gTex = cmds.listConnections(i, type='file')
-    
-    for j in gTex:
-        toSwitch = cmds.getAttr('%s.fileTextureName' % j)
-        cmds.setAttr(('%s.fileTextureName' % j),(toSwitch.rsplit('.', 1)[0]+'.'+str(gExt)),type='string')
+    #Loop Material Textures & Switch File Extensions
+    for i in gMtl:
+        gTex = cmds.listConnections(i, type='file')
+
+        for j in gTex:
+            toSwitch = cmds.getAttr('%s.fileTextureName' % j)
+            cmds.setAttr(('%s.fileTextureName' % j),(toSwitch.rsplit('.', 1)[0]+'.'+str(gExt)),type='string')
